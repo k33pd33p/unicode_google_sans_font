bf3276938d9d42c3d48f2d6b20f39c761e9c8a12
nongthaihoang <nongthaihoang@gmail.com>
2019-03-24 22:31:03 +0700

    update

--- common/install.sh
+++ common/install.sh
@@ -1,16 +1,13 @@
-hf () { cp -r $INSTALLER/custom/HF/* $INSTALLER/system/fonts; }
-bf () { cp -r $INSTALLER/custom/BF/* $INSTALLER/system/fonts; }
-cf () { cp -r $INSTALLER/custom/CF/* $INSTALLER/system/fonts; }
+hf () { cp -R $INSTALLER/custom/HF/* $INSTALLER/system/fonts; }
+bf () { cp -R $INSTALLER/custom/BF/* $INSTALLER/system/fonts; }
+cf () { cp -R $INSTALLER/custom/CF/* $INSTALLER/system/fonts; }
 full () { hf; bf; cf; }
-alt () { cp -r $INSTALLER/custom/alt/* $INSTALLER/system/fonts; }
-orig () { cp -r $INSTALLER/custom/orig/* $INSTALLER/system/fonts; }
-xml () { cp -r $INSTALLER/custom/xml/* $INSTALLER/system/; }
-
-PART=1
-VER=1
-XML=false
+alt () { cp -R $INSTALLER/custom/alt/* $INSTALLER/system/fonts; }
+orig () { cp -R $INSTALLER/custom/orig/* $INSTALLER/system/fonts; }
+xml () { cp -R $INSTALLER/custom/xml/* $INSTALLER/system/; }
 
 # 1st selection ---------------------------------------- 
+PART=1
 ui_print " "
 ui_print "- Which part of the system font do you want to replace?"
 ui_print "  Vol+ = Select; Vol- = Ok"
@@ -36,35 +33,39 @@ ui_print " "
 ui_print "* Selected: $PART"
 
 # 2nd selection ---------------------------------------- 
-ui_print " "
-ui_print "- Which version do you want to install?"
-ui_print "  Vol+ = Select; Vol- = OK"
-ui_print " "
-ui_print "  1. Default"
-ui_print "  2. Alternative"
-ui_print "  3. Original"
-ui_print " "
+VER=1
+if [ $PART != 2 ]; then
+	ui_print " "
+	ui_print "- Which version do you want to install?"
+	ui_print "  Vol+ = Select; Vol- = OK"
+	ui_print " "
+	ui_print "  1. Default"
+	ui_print "  2. Alternative"
+	ui_print "  3. Original"
+	ui_print " "
 
-ui_print "  Select:"
-while true; do
-	ui_print "  $VER"
-	if $VKSEL; then
-		VER=$((VER + 1))
-	else 
-		break
-	fi
-	if [ $VER -gt 3 ]; then
-		VER=1
-	fi
-done
+	ui_print "  Select:"
+	while true; do
+		ui_print "  $VER"
+		if $VKSEL; then
+			VER=$((VER + 1))
+		else 
+			break
+		fi
+		if [ $VER -gt 3 ]; then
+			VER=1
+		fi
+	done
 
-ui_print " "
-ui_print "* Selected: $VER"
+	ui_print " "
+	ui_print "* Selected: $VER"
+fi
 
 # 3rd selection ---------------------------------------- 
+XML=false
 ui_print " "
-ui_print "- Include fontxml? (No if not sure)"
-ui_print "  Vol+ = Yes; Vol- = No"
+ui_print "- Include fontxml?"
+ui_print "  Vol+ = Yes; Vol- = No/Not sure"
 ui_print " "
 if $VKSEL; then
 	XML=true	
@@ -75,29 +76,28 @@ fi
 
 ui_print " "
 ui_print "- Proceed?"
-ui_print "  Vol+ = Sure; Vol- = Cancel and Exit"
+ui_print "  Vol+ = Sure; Vol- = Cancel/Exit"
 ui_print " "
 if $VKSEL; then
-	ui_print "* Done!"	
+	ui_print "* Sure"	
 else
-	ui_print "* Exited!"	
-	exit
+	abort "* Exit..."	
 fi
 
 # installation  ---------------------------------------- 
 case $PART in
 	1 ) full;;
-	2 ) hf;;
-	3 ) hf; bf;;
+	2 ) hf; sed -ie 3's/$/-hf&/' $INSTALLER/module.prop;;
+	3 ) hf; bf; sed -ie 3's/$/-hbf&/' $INSTALLER/module.prop;;
 esac
 
 case $VER in
-	2 ) alt;;
-	3 ) orig;;
+	2 ) alt; sed -ie 3's/$/-alt&/' $INSTALLER/module.prop;;
+	3 ) orig; sed -ie 3's/$/-orig&/' $INSTALLER/module.prop;;
 esac
 
 if $XML; then
-	xml
+	xml; sed -ie 3's/$/-xml&/' $INSTALLER/module.prop;
 fi
 
 ui_print " "
